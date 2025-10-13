07e6801e9536b2631e26c51734e61989e51f1d9b
Bob Sponge <spongebobsp22@gmail.com>
2025-10-13 15:33:51 +0300

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zpmprintedfrmtempl.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -0,0 +1,38 @@
+sap.ui.define(['sap/m/MessageToast'], function (MessageToast) {
+  'use strict';
+
+  return {
+    onInit: function (oEvent) {
+      this.extensionAPI.attachPageDataLoaded(this.onDetailsLoaded.bind(this), this);
+    },
+    onDetailsLoaded: function (event, this1) {
+      var controlAttachmentGenerated = this.getView().byId(
+        sap.ui
+          .getCore()
+          .byFieldGroupId('')
+          .find((element) => element.sId.includes('AttachmentsGenerated')).sId,
+      );
+
+      var oView = this.getView();
+      var oBindingContext = oView.getBindingContext();
+
+      if (!event.context.getObject().uuid) {
+        controlAttachmentGenerated.setVisible(false);
+      } else {
+        if (!event.context.getObject().uuid_char) {
+          oView.getModel().setProperty('uuid_char', oBindingContext.getProperty('uuid'), oBindingContext);
+        }
+        controlAttachmentGenerated.setVisible(true);
+      }
+    },
+    override: {
+      onAfterActionExecution: function (sActionName, aSelectedContexts, oResponse) {
+        if (sActionName === 'YourActionName') {
+          // Custom logic after BOPF action
+          sap.m.MessageToast.show('Action executed successfully!');
+          this.extensionAPI.refresh(); // Refresh UI if needed
+        }
+      },
+    },
+  };
+});
